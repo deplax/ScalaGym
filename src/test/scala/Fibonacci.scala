4a73a91ec1ba3b975fa6665b011a889b07fcd63f
deplax <sturdyegg@gmail.com>
2018-08-27 15:05:52 +0900

    [feat]fibonacci

---
+++ src/test/scala/Fibonacci.scala
@@ -0,0 +1,25 @@
+object Fibonacci {
+
+  def fib(n: Int) = {
+    def go(first: Int, second: Int, count: Int): Int = {
+      if (count < 0)
+        first
+      else
+        go(second, first + second, count - 1)
+    }
+
+    if (n == 1) {
+      0
+    } else if (n == 2) {
+      1
+    } else {
+      go(0, 1, n - 2)
+    }
+
+  }
+
+  def main(args: Array[String]) = {
+    /* 0 1 1 2 3 5 8 13 */
+    print(fib(8))
+  }
+}
